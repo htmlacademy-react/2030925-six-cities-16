6e50d3718c0fe2b5c913055408686dd31d3ff8ae
Georgiy Sosedov <MarmeladOFForiginal@mail.ru>
2024-07-17 20:22:12 +0500

    Добавил маршрутизацию

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,46 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from '../../pages/main-page';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import FavoritesPage from '../../pages/favorites-page';
+import LoginPage from '../../pages/login-page';
+import OfferPage from '../../pages/offer-page';
+import ErrorPage from '../../pages/error-page';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
     placesCount: number;
+    authorizationStatus: AuthorizationStatus;
 }
 
-export default function App({placesCount}: AppProps): JSX.Element {
+export default function App({placesCount, authorizationStatus}: AppProps): JSX.Element {
   return(
-    <MainPage placesCount={placesCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage placesCount={placesCount}/>}
+        />
+        <Route
+          path={AuthorizationStatus.Auth ? AppRoute.Favorites : AppRoute.Login}
+          element={
+            <PrivateRoute authorizationStatus={authorizationStatus}>
+              <FavoritesPage/>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage/>}
+        />
+        <Route
+          path={`${AppRoute.Offer}/:id`}
+          element={<OfferPage/>}
+        />
+        <Route
+          path={AppRoute.Error}
+          element={<ErrorPage/>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { Cards } from './mock/cards';
+import { AuthorizationStatus } from './const';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     <App
       placesCount={Cards.length}
+      authorizationStatus={AuthorizationStatus.NoAuth}
     />
   </React.StrictMode>
 );
